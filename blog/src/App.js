382188b234fd8e79a55bf9a4959ebc4393e80c79
boxer <aszzoon@naver.com>
2021-10-07 13:30:58 +0900

    state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = 'MLB'
+  let [글제목,글제목변경] = useState('남자 코트 추천');
 
+  let posts = '강남고기맛집';
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ { color : 'black', fontSize : 30} } >프론트엔드 개발자</div>
+        <div>프론트엔드 개발자</div>
       </div>
-      <h1> { posts } </h1>
+      <div className="list">
+      <h1> { 글제목 } </h1>
+      <p>2월 17일 발행</p>
+      <hr/>
+      </div>
     </div>
   );
 }
