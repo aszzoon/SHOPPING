a13a3ea1317459072306def8b1485a5f5e3ed819
boxer <aszzoon@naver.com>
2021-10-08 18:43:12 +0900

    m3

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,31 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [글제목, 글제목변경] = useState(['코트', '구두', '속옷']);
+  let [하트, 하트변경] = useState(0);
+  let posts = '강남 고기 맛집';
 
-  let [글제목,글제목변경] = useState('남자 코트 추천');
-
-  let posts = '강남고기맛집';
   return (
     <div className="App">
       <div className="black-nav">
-        <div>프론트엔드 개발자</div>
+        <div>Slim Cloth</div>
       </div>
       <div className="list">
-      <h1> { 글제목 } </h1>
+      <h3> { 글제목[0] } <span onClick={ ()=> { 하트변경(하트 + 1)} }>❤️</span> {하트} </h3>
       <p>2월 17일 발행</p>
+      <hr/>
+      </div>
+      <div className="list">
+      <h3> { 글제목[1] } </h3>
+      <p>2월 18일 발행</p>
+      <hr/>
+      </div>
+      <div className="list">
+      <h3> { 글제목[2] } </h3>
+      <p>2월 19일 발행</p>
       <hr/>
       </div>
     </div>
